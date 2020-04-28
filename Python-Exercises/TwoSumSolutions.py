6c338870ee3a1887a4baab2cd2272146a5bd0be2
Esteban Peralta <54221172+peraltae@users.noreply.github.com>
2020-04-27 20:09:00 -0700

    Add files via upload

--- Python-Exercises/TwoSumSolutions.py
+++ Python-Exercises/TwoSumSolutions.py
@@ -32,9 +32,6 @@ class TwoSumSolutions:
         for i, num in enumerate(nums):
             #Find the compliment of the target and num at enumeration i
             compliment = target - num
-
-			#If the compliment of the target minus the current num isn't in the dictionary, then add it with 
-            # the nums[i] as key and index as the value
 			
             if compliment not in checker:
                 checker[num] = i  # if the compliment is not in the dict, then num:i is added
