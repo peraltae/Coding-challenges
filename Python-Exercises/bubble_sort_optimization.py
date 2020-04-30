6aa3b8b8793f7123de9bb69af093d2599140263e
Esteban Peralta <54221172+peraltae@users.noreply.github.com>
2020-04-30 12:36:20 -0700

    Update bubble_sort_optimization.py

--- Python-Exercises/bubble_sort_optimization.py
+++ Python-Exercises/bubble_sort_optimization.py
@@ -6,7 +6,6 @@ to their final correct positions at the top X highest slots in the array. So, th
 is no need for each sweep to work over the entire array: later sweeps can stop earlier. 
 
 Figure out how to trip the inner loop with successive passes, but without affecting the accuracy of the sorting.
---> focus on inner for-loop so that after each pass, I'm not going through the etire list 
 
 2) Bubble Sort has a very nice property: if the list is already partially sorted, Bubble Sort doesn’t need 
 the entire N sweeps to sort N items. Specifically, it can stop the outer loop earlier under some conditions. 
@@ -21,19 +20,18 @@ On an array that is actually completely ordered, Bubble Sort should be able to s
 
 def bubble_sort(nums):
 
-	inner_count = 0
+	sweep = 0
 	end = len(nums) - 1
 
 	for rounds in range(len(nums)):
-
-		for i in range(0, end):
+		for i in range(end - sweep):
 			print(end)
 
 			if nums[i] > nums[i+1]:
 				nums[i], nums[i + 1] = nums[i+1], nums[i]
 				end = i+1	
 		
-
+		sweep += 1
 		print(nums, "\n")
 
 	print("\n", inner_count)
