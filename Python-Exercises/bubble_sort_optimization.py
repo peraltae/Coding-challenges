b4e70c1cbdfe395f236608a564b365c9d58479f7
Esteban Peralta <54221172+peraltae@users.noreply.github.com>
2020-04-30 17:47:39 -0700

    Add files via upload

--- Python-Exercises/bubble_sort_optimization.py
+++ Python-Exercises/bubble_sort_optimization.py
@@ -6,12 +6,15 @@ to their final correct positions at the top X highest slots in the array. So, th
 is no need for each sweep to work over the entire array: later sweeps can stop earlier. 
 
 Figure out how to trip the inner loop with successive passes, but without affecting the accuracy of the sorting.
+--> Continually deduct the number of rounds completed from the length of iteration of the inner for-loop
 
 2) Bubble Sort has a very nice property: if the list is already partially sorted, Bubble Sort doesn’t need 
 the entire N sweeps to sort N items. Specifically, it can stop the outer loop earlier under some conditions. 
 
 Figure out what that condition is, and see if you can get it to run much faster on mostly-ordered arrays. 
 On an array that is actually completely ordered, Bubble Sort should be able to stop after a single sweep.
+
+--> Assume that the array is partially sorted, and continue swapping if a contridiction is found
 
 
 '''
@@ -20,26 +23,38 @@ On an array that is actually completely ordered, Bubble Sort should be able to s
 
 def bubble_sort(nums):
 
-	sweep = 0
-	end = len(nums) - 1
+
+	outer_loop_count = 0
+	inner_loop_count = 0
+ 
 
 	for rounds in range(len(nums)):
-		for i in range(end - sweep):
-			print(end)
+		sorted = True
 
+		for i in range(len(nums)-1-rounds):
 			if nums[i] > nums[i+1]:
 				nums[i], nums[i + 1] = nums[i+1], nums[i]
-				end = i+1	
+				sorted = False
+				inner_loop_count += 1
+
 		
-		sweep += 1
-		print(nums, "\n")
+		outer_loop_count += 1
 
-	print("\n", inner_count)
+		if(sorted):
+			break
 
 
-def main():
-	f = [3,4,5,2,1]
 
-	bubble_sort(f)
+		
 
-main()
+	#print statements for debugging
+	print("outer-loops done:", outer_loop_count)
+	print("inner-loops done:", inner_loop_count)
+	print(nums, "\n")
+
+
+
+
+
+
+
